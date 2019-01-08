251f792c39a3635d27630797bfb34a44ff1b3802
Daniel Keen <keendaniel01@gmail.com>
2019-01-08 17:00:22 +0000

    Add jwt auth

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,7 +1,18 @@
 const { forwardTo } = require('prisma-binding');
 
 const Query = {
-	cryptoCurrencies: forwardTo('db')
+	cryptoCurrencies: forwardTo('db'),
+	me(parent, args, ctx, info) {
+		if (!ctx.request.userId) {
+			return null;
+		}
+		return ctx.db.query.user(
+			{
+				where: { id: ctx.request.userId }
+			},
+			info
+		);
+	}
 };
 
 module.exports = Query;
--- frontend/components/Nav/index.js
+++ frontend/components/Nav/index.js
@@ -1,8 +1,16 @@
 import Link from 'next/link';
 import NavStyles from './style';
+import User from '../User';
 
 const Nav = () => (
 	<NavStyles data-test="nav">
+		<User>
+			{({ data: { me } }) => {
+				console.log(me);
+				if (me) return <p>{me.name}</p>;
+				return null;
+			}}
+		</User>
 		<Link href="/signup">
 			<a>Sign up</a>
 		</Link>
