bdf892407a6fdf4bbc4fc7be3a0bee7dac323e75
Daniel Keen <keendaniel01@gmail.com>
2019-01-08 13:59:16 +0000

    (feat) Backend signup flow

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const Mutations = {
 	async createCryptoCurrency(parent, args, ctx, info) {
 		// TODO: Check if they're logged in
@@ -12,6 +15,27 @@ const Mutations = {
 		);
 
 		return cryptoCurrency;
+	},
+	async signup(parent, args, ctx, info) {
+		args.email = args.email.toLowerCase();
+		// hash password
+		const password = await bcrypt.hash(args.password, 10);
+		const user = await ctx.db.mutation.createUser(
+			{
+				data: {
+					...args,
+					password,
+					permissions: { set: [ 'USER' ] }
+				}
+			},
+			info
+		);
+		const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+		ctx.response.cookie('token', token, {
+			httpOnly: true,
+			maxAge: 1000 * 60 * 60 * 24 * 365
+		});
+		return user;
 	}
 };
 
